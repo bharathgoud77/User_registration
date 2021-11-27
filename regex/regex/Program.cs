71b7561bb5cb0d29de81c7de578287a2ca97e60b
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-27 19:13:55 +0530

    uc1

---
+++ regex/regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^[9][1][7-9]{1}[0-9]{9}$";
+
+        public bool validatePinCode(string PinCode)
+        {
+            return Regex.IsMatch(PinCode, REGEX_PINCODE);
+        }
+    }
+}
---
+++ regex/regex/regex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string path = "918014123456";
+            Patterns obj = new Patterns();
+            checkvalue = obj.validatePinCode(path);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern Match");
+            }
+            else
+            {
+                Console.WriteLine("Pattern NOt Matched");
+            }
+        }
+    }
+}
